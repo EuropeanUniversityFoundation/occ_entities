2dda960cda1efb38d0f0a83d86d878a36b4f8f7a
groszgabor <ittisvagyok@gmail.com>
2025-06-23 10:57:23 +0200

    Modifies validator to show error path (needs further work, not a priority now)

--- src/Plugin/Validation/Constraint/YearFormatConstraintValidator.php
+++ src/Plugin/Validation/Constraint/YearFormatConstraintValidator.php
@@ -3,6 +3,7 @@
 namespace Drupal\occ_entities\Plugin\Validation\Constraint;
 
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Util\PropertyPath;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class YearFormatConstraintValidator extends ConstraintValidator
@@ -12,20 +13,24 @@ class YearFormatConstraintValidator extends ConstraintValidator
 
   public function validate($value, Constraint $constraint)
   {
-    foreach ($value as $item) {
+    foreach ($value as $delta => $item) {
       $year_value = $item->{self::SUBFIELD_NAME};
       if (!preg_match('~^\d+/\d+$~', $year_value)) {
         /** @var YearFormatConstraint $constraint */
-        $this->context->addViolation($constraint->noRegexMatchMessage, [
-          '%value' => $year_value,
-        ]);
+        $this->context->buildViolation($constraint->noRegexMatchMessage)
+          ->setParameter('%value', $year_value)
+          ->atPath($delta)
+          ->addViolation();
       }
 
       if (str_contains($year_value, '/')) {
         $year_values = explode('/', $year_value);
         if ($year_values[0] > $year_values[1]) {
           /** @var YearFormatConstraint $constraint */
-          $this->context->addViolation($constraint->numberValueMismatchMessage, ['%value' => $year_value]);
+          $this->context->buildViolation($constraint->numberValueMismatchMessage)
+            ->setParameter('%value', $year_value)
+            ->atPath($delta)
+            ->addViolation();
         }
       }
     }
