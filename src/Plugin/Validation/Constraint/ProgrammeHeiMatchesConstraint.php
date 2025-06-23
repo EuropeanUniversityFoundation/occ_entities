a8bc52ce0d20260c51678be151610ffa05434c1f
groszgabor <ittisvagyok@gmail.com>
2025-06-23 10:55:11 +0200

    Adds programme_hei_matches constraint and validator

---
+++ src/Plugin/Validation/Constraint/ProgrammeHeiMatchesConstraint.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\occ_entities\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Provides a ProgrammeHeiMatches constraint.
+ *
+ * @Constraint(
+ *   id = "programme_hei_matches",
+ *   label = @Translation("ProgrammeHeiMatches", context = "Validation"),
+ * )
+ *
+ * @DCG
+ * To apply this constraint on third party entity types implement either
+ * hook_entity_base_field_info_alter() or hook_entity_bundle_field_info_alter().
+ *
+ * @see https://www.drupal.org/node/2015723
+ */
+final class ProgrammeHeiMatchesConstraint extends Constraint
+{
+
+  public string $message = '%programme must reference the same HEI as this course';
+}
---
+++ src/Plugin/Validation/Constraint/ProgrammeHeiMatchesConstraintValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\occ_entities\Plugin\Validation\Constraint;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates the ProgrammeHeiMatches constraint.
+ */
+final class ProgrammeHeiMatchesConstraintValidator extends ConstraintValidator
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate(mixed $items, Constraint $constraint): void
+  {
+    if (!$items instanceof FieldItemListInterface) {
+      throw new \InvalidArgumentException(
+        sprintf('The validated value must be instance of \Drupal\Core\Field\FieldItemListInterface, %s was given.', get_debug_type($items))
+      );
+    }
+
+    $referenced_hei_target_ids = $items->getEntity()->get('hei')->getValue();
+    foreach ($items as $delta => $item) {
+      $referenced_entity_hei_target_ids = $item->entity->hei->getValue();
+
+      if ($referenced_hei_target_ids !== $referenced_entity_hei_target_ids) {
+        /** @var ProgrammeHeiMatchesConstraint $constraint */
+        $this->context->buildViolation($constraint->message)
+          ->setParameter('%programme', $item->entity->label())
+          ->atPath($delta)
+          ->addViolation();
+      }
+    }
+  }
+}
