f3cde0f136b0b6fa2935134c7d938e2c365a2329
groszgabor <ittisvagyok@gmail.com>
2025-06-23 15:44:56 +0200

    Adds int_over_int constraint and validator

---
+++ src/Plugin/Validation/Constraint/IntOverIntConstraintValidator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\occ_entities\Plugin\Validation\Constraint;
+
+use Drupal\Core\Field\FieldItemList;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class IntOverIntConstraintValidator extends ConstraintValidator
+{
+
+  public function validate($value, Constraint $constraint)
+  {
+    if (!$value instanceof FieldItemList) {
+      throw new \InvalidArgumentException(
+        sprintf('The validated value must be instance of \Drupal\Core\Field\FieldItemList, %s was given.', get_debug_type($value))
+      );
+    }
+
+    if ($value->isEmpty()) {
+      return;
+    }
+
+    foreach ($value as $delta => $item) {
+      $field_value = $item->get('value')->getValue();
+      if (!preg_match('~^\d+/\d+$~', $field_value)) {
+        /** @var IntOverIntConstraint $constraint */
+        $this->context->buildViolation($constraint->noRegexMatchMessage)
+          ->setParameter('%value', $field_value)
+          ->atPath($delta)
+          ->addViolation();
+      }
+
+      if (str_contains($field_value, '/')) {
+        $field_values = explode('/', $field_value);
+        if ($field_values[0] > $field_values[1]) {
+          /** @var IntOverIntConstraint $constraint */
+          $this->context->buildViolation($constraint->numberValueMismatchMessage)
+            ->setParameter('%value', $field_value)
+            ->atPath($delta)
+            ->addViolation();
+        }
+      }
+    }
+  }
+}
